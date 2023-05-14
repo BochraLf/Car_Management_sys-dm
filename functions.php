801c84d2a82b40955ee786fa055c548ba84dc128
Amine-Agg <mohamed_amine.aggoun@g.enp.edu.dz>
2023-05-14 21:04:08 +0200

    Some Beauty :b

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 function pdo_connect_mysql() {
     $DATABASE_HOST = 'localhost';
     $DATABASE_USER = 'root';
-    $DATABASE_PASS = '291036250';
+    $DATABASE_PASS = '1233';
     $DATABASE_NAME = 'car';
     try {
     	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
--- index.php
+++ index.php
@@ -1,16 +1,32 @@
 <?php
 include 'functions.php';
+
 // Your PHP code here.
 
 // Home Page template below.
 ?>
-
+<link rel="stylesheet" type="text/css" href="style.css">
 <?=template_header('CRentals')?>
 
-<div class="content" style="text-align: center;">
-  <h1 style="font-size: 50px; ">Car Rental Management System <br> </h1>
-  <p>haba ykoun kayen slide des images tae des voitures hna + des animations </p>
-
+<div  >
+  <div class="content" style="text-align: center ; ">
+    <h1 style="font-size: 50px; margin-top: 20px; ">Car Rental Management System <br> </h1>
+   
+      <div class="container" style="margin-top: 80px;">
+      <div>
+        <p class="text-wrapper">Drive into your next Adventure &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;with us</p>
+        </div>
+        <img src="icons/car04.png" alt="car" class="car-image">
+       
+       </div>
+  </div>
 </div>
+
+<style>
+  body{
+    background-color: #BAD7E9;
+  }
+</style>
+
 
 <?=template_footer()?>
